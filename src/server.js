b2826d2051f42e30427f24badd307553e27b57e2
CalebeLAR <ca6ele@gmail.com>
2023-02-19 22:51:15 -0400

    endpoint /teams

--- src/server.js
+++ src/server.js
@@ -1,17 +1,3 @@
 const app = require('./app');
 
-// const teams = [
-//   {
-//     id: 1,
-//     name: 'São Paulo Futebol Clube',
-//     initials: 'SPF',
-//   },
-//   {
-//     id: 2,
-//     name: 'Clube Atlético Mineiro',
-//     initials: 'CAM',
-//   },
-// ];
-
-app.listen(3001, () => console.log('server running on port 3001'));
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.listen(3000, () => console.log('server running on port 3000'));
